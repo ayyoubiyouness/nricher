2c5e3c35626b8c521c67f09572f4f963e4f07a27
Ayyoubi <114993701+ayyoubiyouness@users.noreply.github.com>
2023-07-06 04:49:50 -0800

    Create color.R
    Ce code permet de trouver la couleur dans un text

---
+++ color.R
@@ -0,0 +1,22 @@
+couleurs <- c("rouge", "bleu", "vert", "jaune", "orange", "violet", "rose", "noir", "blanc", "gris", "marron",
+              "cyan", "argent", "or", "turquoise", "indigo", "magenta", "bronze", "beige", "olive", "aubergine", "ivoire")
+
+datacolor <-  as.data.frame(couleurs)
+
+findColor <- function(inputString ) {
+  inputString <- tolower(inputString)
+  matchedColor <- NA
+  for (i in 1:nrow(datacolor)) {
+    category <- tolower(datacolor$couleurs[i])
+    if ( grepl(category, inputString) ) {
+      matchedColor <- category
+      return( paste("ce texte contient la couleur : ",category))
+      break
+    }
+  }
+  return(print("pas de couleur"))
+}
+
+
+description <- "Matelas  mousse 140x190 cm"
+findColor("Matelas  mousse 140x190 cm")
