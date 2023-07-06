2a5b51ebd8e2cbbd97c7cf2379b0154523cf8e91
Ayyoubi <114993701+ayyoubiyouness@users.noreply.github.com>
2023-07-06 05:24:28 -0800

    Create ColDim.R
    ce code permet d'extraire la dimension et la couleur dans un texte

---
+++ ColDim.R
@@ -0,0 +1,60 @@
+# code pour extraire la couleur dans une chaine 
+couleurs <- c("rouge", "bleu", "vert", "jaune", "orange", "violet", "rose", "noir", "blanc", "gris", "marron",
+              "cyan", "argent", "or", "turquoise", "indigo", "magenta", "bronze", "beige", "olive", "aubergine", "ivoire")
+
+datacolor <-  as.data.frame(couleurs)
+
+findColor <- function(inputString ) {
+  inputString <- tolower(inputString)
+  matchedColor <- NA
+  for (i in 1:nrow(datacolor)) {
+    category <- tolower(datacolor$couleurs[i])
+    if ( grepl(category, inputString) ) {
+      matchedColor <- category
+      return(category)
+      break
+    }
+  }
+  return(NA)
+}
+
+
+
+findColor("Matelas rouge   mousse cm")
+
+# code pour determiner la dimension
+findDimension <- function(chaine) {
+  dimension <- regmatches(chaine, regexpr("\\d+\\s*[xX]\\s*\\d+", chaine))
+  if (length(dimension) > 0) {
+    dimension <- gsub("\\s", "", dimension)  
+    return( dimension)
+  } else {
+    return(NA)
+  }
+}
+
+
+chaine <- "Matelas 140x190 mousse  cm"
+
+
+findDimension(chaine)
+
+# code pour extraire la couleur et la dimension dans une chaine 
+ColDim <- function(chaine) {
+  a <- findDimension(chaine)
+  b <- findColor(chaine)
+  if(is.na(a) & is.na(b)) {
+    return(paste("Ce texte ne présente pas de dimension ni de couleur"))
+  } else if ( !is.na(a) & !is.na(b) ) {
+    return(paste("Dans ce texte, il y a la couleur", b, " et la dimension", a))
+  } else if( !is.na(a) & is.na(b) ) {
+    return(paste("Ce texte présente la dimension", a))
+  } else if ( is.na(a) & !is.na(b) ) {
+    return(paste(" ce texte présente la couleur", b))
+  }
+}
+
+
+chaine <- "Matelas mousse rouge 140x190   cm"
+
+ColDim(chaine)
