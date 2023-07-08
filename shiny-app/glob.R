a7e02f33b5ea8cc8ca5d9225dd62f346e1d3167d
Ayyoubi <114993701+ayyoubiyouness@users.noreply.github.com>
2023-07-08 13:41:58 -0800

    Update glob.R

--- shiny-app/glob.R
+++ shiny-app/glob.R
@@ -4,6 +4,8 @@ library(highcharter)
 library(shinydashboard)
 library(readxl)
 
+# ce script est utilisé à la fois dans l'app shiny et pour la préparation des données 
+# d'entrainement du modèle
 
 # Filtration de data par vendeur
 
@@ -39,20 +41,22 @@ data_autre_vendeur$`Date de commande`<- as.Date(data_autre_vendeur$`Date de comm
 data_autre_vendeur  <- data_autre_vendeur[order(data_autre_vendeur$`Date de commande`), ]
 
 
+
+
+
 # préparation des données de ventes pour chaque jour de chaque vendeur
 
+# La fonction permet de donner la somme des valeurs de la variable dans la data pour chaque jour
 calculate_sum_by_date <- function(data, variable) {
   result <- as.data.frame(by(data[[variable]], list(data$`Date de commande`), sum))
   Date <- unique(data$`Date de commande`)
   result <- cbind(result, Date)
   colnames(result) <- c( "Montant", "Date")
-  # type_vend <-as.data.frame(rep(data[1,3], nrow(result)))
-  # colnames(type_vend) <- data[1,3]
-  # result <- cbind(type_vend, result  )
   return(result)
   
 }
 
+# Je calcule le prix de vente de chaque vendeur par jour
 vend1_sum <- calculate_sum_by_date(data_vendeur1, "Montant cmd" )
 type_vend <-as.data.frame(rep("Vendeur 1", nrow(vend1_sum)))
 colnames(type_vend) <- "Vendeur"
@@ -91,5 +95,145 @@ type_vend <-as.data.frame(rep("Autre vendeur", nrow(vendautre_sum)))
 colnames(type_vend) <- "Vendeur"
 vendautre_sum <- cbind(type_vend, vendautre_sum  )
 
-
+# je rassemble les données dans une matrice pour faciliter la visualisation 
 datavend <- rbind(vend1_sum, vend2_sum, vend3_sum, vend4_sum, vend5_sum, vend6_sum, vendautre_sum)
+
+
+
+# maintenant on filtre les données pour matelas
+
+datamatelas <- filter(datap, datap$Nature == "Matelas")
+# je détermine la quantité vendue des matelats de chaque vendeur 
+result <- as.data.frame(by(datamatelas$Quantité, list(datamatelas$`Vendeur`), sum))
+a <- as.data.frame(rownames(result))
+quant_matela <- cbind(a, result)
+colnames(quant_matela) <- c("Vendeur", "Quantité")
+# je détermine le prix de vente des matelats pour chaque vendeur
+result2 <- as.data.frame(by(datamatelas$`Montant cmd`, list(datamatelas$`Vendeur`), sum))
+aa <- a <- as.data.frame(rownames(result))
+prix_matela <- cbind(aa, result2)
+colnames(prix_matela) <- c("Vendeur", "prix")
+
+
+
+
+# préparatin des données pour l'entrainement 
+# l'idée est de préparer une data de deux colonnes libelle et nature 
+# et que dans la colonne nature on trouve tous les natures présentes dans le dataset 
+
+# fonction pour déterminer la catègorie et retourne na s'il arrive pas à catégoriser
+
+nature_unique <- as.data.frame(unique(data$Nature))
+colnames(nature_unique) <- c("Nature")
+# il y a 596 types de natures
+findCategory <- function(inputString) {
+  inputString <- tolower(inputString)
+  matchedCategory <- NA
+  for (i in 1:nrow(nature_unique)) {
+    category <- tolower(nature_unique$Nature[i])
+    if (str_detect(inputString, paste0("\\b", category, "\\b"))) {
+      matchedCategory <- nature_unique$Nature[i]
+      break
+    }
+  }
+  return(matchedCategory)
+}
+
+# filtrer les données pour avoir juste les seuls libelles avec
+# natures correspondantes de la data d'origine
+datapre <- select(data, c(2,5))
+filtered_datapre <-  subset(datapre, !duplicated(`Libellé produit`))
+# il y a 55997 produits présents dans le dataset
+print(filtered_datapre)
+
+
+# Pour déterminer le nombre de libelle que l'algo permet de catégoriser
+df_test <- data.frame()
+for (i in 1:nrow(filtered_datapre)) {
+  lib <- filtered_datapre$`Libellé produit`[i]
+  matchedCategory <- findCategory(lib)
+  category <- filtered_datapre$Nature[i]
+  if (!is.na(matchedCategory)  && (matchedCategory == category) ) {
+    print(i)
+    df_test <- rbind(df_test, i)
+    filtered_datapre$Nature[i] <- matchedCategory
+  }
+  
+}
+colnames(df_test) <- c("indice")
+
+# La dimension de df_test est 20011 il reste donc 35986
+
+# on choisit les données qui sont bien classés par l'algo
+# Dans cette data on a 342 natures uniques, il reste donc 254
+filtered_datap <- filtered_datapre[df_test$indice, ]
+
+
+
+# on choisit les données que l'algo n'arrive pas à classer
+filtered_datan <- filtered_datapre[-df_test$indice, ]
+
+# Maintenant je vais faire des traitements sur les données pour que l'algo 
+# puisse les catégoriser, l'objectif est d'avoir plus de nature 
+# pour passer à l'entrainelent du modèle 
+# Majuscule to minuscule
+filtered_datan <- filtered_datan %>% mutate(Nature = tolower(Nature))
+filtered_datan <- filtered_datan %>% mutate(`Libellé produit` = tolower(`Libellé produit`))
+
+# on remplace les caractères spéciaux par leurs équivalents normaux 
+# j'utilise des fonctions d'expression régulière 
+replacetext <- function(text) {
+  text <- gsub("é", "e", text, ignore.case = TRUE)
+  text <- gsub("è", "e", text, ignore.case = TRUE)
+  text <- gsub("à", "a", text, ignore.case = TRUE)
+  return(text)
+}
+
+# Maintenent j'applique cette fonction au dataset qu'on veut catégoriser
+for (i in 1:nrow(filtered_datan)) {
+  text1 <- filtered_datan$`Libellé produit`[i]
+  text2 <- filtered_datan$Nature[i]
+  text1 <- replacetext(text1)
+  text2 <- replacetext(text2)
+  filtered_datan$`Libellé produit`[i] <- text1
+  filtered_datan$Nature[i] <- text2
+  print(i)
+  
+}
+
+
+
+
+# Ensuite, on refait la catégorisation des produits
+
+nature_unique2 <- as.data.frame(unique(filtered_datan$Nature))
+
+findCategory2 <- function(inputString) {
+  inputString <- tolower(inputString)
+  matchedCategory <- NA
+  for (i in 1:nrow(nature_unique2)) {
+    category <- tolower(nature_unique2$Nature[i])
+    if (str_detect(inputString, paste0("\\b", category, "\\b"))) {
+      matchedCategory <- nature_unique2$Nature[i]
+      break
+    }
+  }
+  return(matchedCategory)
+}
+
+
+df_test2 <- data.frame()
+for (i in 1:nrow(filtered_datan)) {
+  
+  lib <- filtered_datan$`Libellé produit`[i]
+  matchedCategory <- findCategory2(lib)
+  category <- filtered_datan$Nature[i]
+  if (!is.na(matchedCategory) && !is.na(category) && matchedCategory != ""   &&  (matchedCategory == category) ) {
+    print(i)
+    df_test2 <- rbind(df_test2, i)
+    filtered_datan$Nature[i] <- matchedCategory
+  }
+  
+}
+colnames(df_test) <- c("indice")
+
